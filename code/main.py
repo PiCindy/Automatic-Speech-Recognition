1641919480e1a4465478f78b143ce6f4e1ca10aa
MadCindy <cindypereira93@gmail.com>
2021-11-23 17:18:10 +0100

    blabla

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
-import lang_mod.decoder_jsgf_1digit
-import lang_mod.decoder_jsgf_3digit
-import lang_mod.decoder_jsgf_5digit
+import lang_mod.decoder_jsgf_digit
 import lang_mod.decoder_jsgf_digitloop
 import lang_mod.decoder_ngram_digitloop
 
 import lang_mod.results
+
+import speakers.decoder_man
+import speakers.results
--- code/speakers/results.py
+++ code/speakers/results.py
@@ -1,16 +1,16 @@
 from jiwer import wer
 import math
 
-list_files = ["digit", "digitloop", "digits_ngram"]
+list_files = ["man"]
 
 for file in list_files:
-    with open(f"results/lang_mod/{file}.ref", 'r') as f:
+    with open(f"results/speakers/{file}.ref", 'r') as f:
         lines = f.readlines()
         ground = [line.rstrip() for line in lines if line != '\n']
-    with open(f"results/lang_mod/{file}.hyp", 'r') as f:
+    with open(f"results/speakers/{file}.hyp", 'r') as f:
         lines = f.readlines()
         hypothesis = [line.rstrip() for line in lines if line != '\n']
-    with open(f"results/lang_mod/{file}.results", 'w') as f:
+    with open(f"results/speakers/{file}.results", 'w') as f:
         error = wer(ground, hypothesis)
         error_percentage = error*100
         confidence = 1.96*math.sqrt(error*(1-error)/len(ground))
