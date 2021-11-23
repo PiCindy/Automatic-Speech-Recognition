27e6e6259204421ec5fbdfc4413b06568deb13fe
MadCindy <cindypereira93@gmail.com>
2021-11-23 17:39:56 +0100

    Exercises 2 and 3

--- code/length/results.py
+++ code/length/results.py
@@ -1,16 +1,16 @@
 from jiwer import wer
 import math
 
-list_files = ["man", 'woman', 'boy', 'girl']
+list_files = ["1digit", '3digit', '5digit']
 
 for file in list_files:
-    with open(f"results/speakers/{file}.ref", 'r') as f:
+    with open(f"results/length/{file}.ref", 'r') as f:
         lines = f.readlines()
         ground = [line.rstrip() for line in lines if line != '\n']
-    with open(f"results/speakers/{file}.hyp", 'r') as f:
+    with open(f"results/length/{file}.hyp", 'r') as f:
         lines = f.readlines()
         hypothesis = [line.rstrip() for line in lines if line != '\n']
-    with open(f"results/speakers/{file}.results", 'w') as f:
+    with open(f"results/length/{file}.results", 'w') as f:
         error = wer(ground, hypothesis)
         error_percentage = error*100
         confidence = 1.96*math.sqrt(error*(1-error)/len(ground))
--- code/main.py
+++ code/main.py
@@ -1,8 +1,15 @@
 import lang_mod.decoder_jsgf_digit
 import lang_mod.decoder_jsgf_digitloop
 import lang_mod.decoder_ngram_digitloop
-
 import lang_mod.results
 
 import speakers.decoder_man
+import speakers.decoder_woman
+import speakers.decoder_boy
+import speakers.decoder_girl
 import speakers.results
+
+import length.decoder_1digit
+import length.decoder_3digit
+import length.decoder_5digit
+import length.results
